8b8ce74678096d83754fc38c8ea6f97cbfdc4ebd
Cédric Ronvel <cedric.ronvel@gmail.com>
2024-09-18 20:03:25 +0200

    wip: Style refacto (still lot of issues)

--- sample/browser-test.js
+++ sample/browser-test.js
@@ -346,12 +346,11 @@ async function viewBoxTest() {
 	} ) ;
 
 	var vgPolygon = new svgKit.VGConvexPolygon( {
-		/*
 		style: {
 			fill: '%lighter green' ,
 			stroke: '%red' ,
 			strokeWidth: 2
-		} ,*/
+		} ,
 		build: {
 			x: 0 ,
 			y: 0 ,
@@ -559,8 +558,8 @@ async function renderCanvas( vg , options ) {
 
 
 
-//svgKit.domKit.ready( test ) ;
-svgKit.domKit.ready( viewBoxTest ) ;
+svgKit.domKit.ready( test ) ;
+//svgKit.domKit.ready( viewBoxTest ) ;
 //svgKit.domKit.ready( pathTest ) ;
 //svgKit.domKit.ready( polygonTest ) ;
 //svgKit.domKit.ready( hexaTilesTest ) ;
