f3e49c88c6eb9d68dbe0376a40f7f3deb3310526
Cédric Ronvel <cedric.ronvel@gmail.com>
2024-09-23 15:41:24 +0200

    wip

--- test/svg-test.js
+++ test/svg-test.js
@@ -78,20 +78,59 @@ describe( "Simple Convex Polygon" , () => {
 
 describe( "Path" , () => {
 
-	it( "Bezier path" , () => {
+	it( "Path's part" , () => {
 		var path = new svgKit.Path() ;
 		//path.curve( { cx1: 3 , cy1: 3 , cx2: 3 , cy2: -3 , x: 6 , y: 0 } ) ;
 		//path.line( { x: 6 , y: 10 } ) ;
 		path.arc( { rx: 4 , ry: 6 , x: 6 , y: 0 } ) ;
-		var curves = path.computeCurves() ;
-		var curve = curves[ 0 ] ;
+		path.computeCurves() ;
+		var curve = path.curves[ 0 ] ;
 		log( "Curve: %I" , curve ) ;
 
 		for ( let i = 0 ; i < curve.length ; i ++ ) {
-			let point = curve.getPointAndTangentAtLength( i ) ;
+			let point = curve.getPropertiesAtLength( i ) ;
 			log( "Point at length %f: %n" , i , point ) ;
 		}
-		log( "Point at length %f: %n" , curve.length , curve.getPointAndTangentAtLength( curve.length ) ) ;
+		log( "Point at length %f: %n" , curve.length , curve.getPropertiesAtLength( curve.length ) ) ;
+	} ) ;
+
+	it( "Path points" , () => {
+		var path = new svgKit.Path() ;
+		path.moveTo( { x: 3 , y: 3 } ) ;
+		path.lineTo( { x: 10 , y: 10 } ) ;
+		path.arcTo( { rx: 7 , ry: 7 , x: 20 , y: 15 } ) ;
+		path.curveTo( { cx1: 23 , cy1: 18 , cx2: 23 , cy2: 12 , x: 26 , y: 15 } ) ;
+		//path.computeCurves() ;
+		//log( "path: %I" , path ) ;
+		var pathLength = path.getLength() ;
+		log( "length: %f" , pathLength ) ;
+
+		//let point = path.getPointAtLength( 0 , true ) ; log( "Point at length %f: %n" , 0 , point ) ; return ;
+
+		for ( let i = 0 ; i < pathLength ; i ++ ) {
+			let point = path.getPointAtLength( i , true ) ;
+			log( "Point at length %f: %n" , i , point ) ;
+		}
+
+		log( "Point at length %f: %n" , pathLength , path.getPointAtLength( pathLength ) ) ;
+	} ) ;
+
+	it( "zzz Path point, using the generator" , () => {
+		var path = new svgKit.Path() ;
+		path.moveTo( { x: 3 , y: 3 } ) ;
+		path.lineTo( { x: 10 , y: 10 } ) ;
+		path.arcTo( { rx: 7 , ry: 7 , x: 20 , y: 15 } ) ;
+		path.curveTo( { cx1: 23 , cy1: 18 , cx2: 23 , cy2: 12 , x: 26 , y: 15 } ) ;
+		//path.computeCurves() ;
+		//log( "path: %I" , path ) ;
+		var pathLength = path.getLength() ;
+		log( "length: %f" , pathLength ) ;
+
+		//let point = path.getPointAtLength( 0 , true ) ; log( "Point at length %f: %n" , 0 , point ) ; return ;
+
+		for ( let data of path.getPointEveryLength( 2 , { forceKeyPoints: true } ) ) {
+			log( "Point at length: %n" , data ) ;
+		}
 	} ) ;
 } ) ;
 
